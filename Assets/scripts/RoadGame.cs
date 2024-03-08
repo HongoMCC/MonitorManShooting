35edeb171ce67c1a104225c93b06c2035b29f944
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-03-08 16:11:24 +0900

    aaaaaa

--- Assets/scripts/RoadGame.cs
+++ Assets/scripts/RoadGame.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 using System;
-using System.collections;
 using UnityEngine.SceneManagement;
 
 public class RoadGame : MonoBehaviour
 {
     public string SceneName;
-    void OnButtonClicked()
+    public void OnButtonClicked()
     {
         SceneManager.LoadScene(SceneName);
     }
