e54f90faf7efd8c666b9d147f9836a064800e0b1
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-09-03 08:56:49 +0900

    文化祭向けに調整

---
+++ Assets/scripts/DontDestoroy.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+using System;
+
+public class DontDestoroy : MonoBehaviour
+{
+    string exepath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        exepath = Path.Combine(Application.dataPath, "../../../HongoMCCGame2024.exe");
+    }
+    void OnApplicationQuit()
+    {
+        try
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo(exepath)
+            };
+            process.Start();
+        }
+        catch(Exception ex)
+        {
+            UnityEngine.Debug.LogException(ex);
+        }
+    }
+}
